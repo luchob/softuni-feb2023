525208af72b50acd4b9d74f96eb511d54c58b8a3
luchob <lachezar.balev@gmail.com>
2023-02-20 19:41:36 +0200

    New books project.

---
+++ books-rest-server/src/main/java/bg/softuni/books/BooksRestServerApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksRestServerApplication.class, args);
+	}
+
+}
