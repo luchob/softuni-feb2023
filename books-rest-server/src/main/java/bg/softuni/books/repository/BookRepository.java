9f2ca4c4d5496053363b537b48d0ac63c526c86a
luchob <lachezar.balev@gmail.com>
2023-02-20 20:01:30 +0200

    Initialize the database.

---
+++ books-rest-server/src/main/java/bg/softuni/books/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.books.repository;
+
+import bg.softuni.books.model.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+}
