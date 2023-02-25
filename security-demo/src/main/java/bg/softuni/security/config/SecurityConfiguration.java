75505de41d307481f87b32ac15245c031fb92822
luchob <lachezar.balev@gmail.com>
2023-02-25 13:16:02 +0200

    Favicon.ico

--- security-demo/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
+++ security-demo/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,7 +32,7 @@ public class SecurityConfiguration {
             loginPage("/users/login").
             usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
             passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-        defaultSuccessUrl("/", true).
+        defaultSuccessUrl("/").
         failureForwardUrl("/users/login-error");
 
     return http.build();
