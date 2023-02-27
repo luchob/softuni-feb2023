619181cfb8550f9ad3b7717d453f8c4bb73ce05a
luchob <lachezar.balev@gmail.com>
2023-02-27 22:03:30 +0200

    Load books with REST API (fetcH).

---
+++ books-js-client/app.js
@@ -0,0 +1,39 @@
+let loadBooksBtn = document.getElementById("loadBooks");
+
+loadBooksBtn.addEventListener('click', reloadBooks)
+
+function reloadBooks() {
+
+  let booksCntr = document.getElementById('books-container')
+  booksCntr.innerHTML = ''
+
+  let requestOptions = {
+    method: 'GET'
+  }
+
+  fetch("http://localhost:8080/api/books", requestOptions).
+    then(response => response.json()).
+    then(json => json.forEach(book => {
+      // create row
+      let row = document.createElement('tr')
+
+      let titleCol = document.createElement('td')
+      let authorCol = document.createElement('td')
+      let isbnCol = document.createElement('td')
+      let actionCol = document.createElement('td')
+
+      //book
+      titleCol.textContent = book.title
+      // atuhor
+      authorCol.textContent = book.author.name
+      isbnCol.textContent = book.isbn
+
+      row.appendChild(titleCol)
+      row.appendChild(authorCol)
+      row.appendChild(isbnCol)
+      row.appendChild(actionCol)
+
+      booksCntr.appendChild(row)
+  }))
+
+}
