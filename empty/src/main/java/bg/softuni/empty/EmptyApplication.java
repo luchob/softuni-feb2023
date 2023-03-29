c9467e8b74d45614a67e92b8fabaef84ae40b2a1
luchob <lachezar.balev@gmail.com>
2023-03-29 20:51:38 +0300

    Empty project

---
+++ empty/src/main/java/bg/softuni/empty/EmptyApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.empty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyApplication.class, args);
+	}
+
+}
---
+++ empty/src/test/java/bg/softuni/empty/EmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.empty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
